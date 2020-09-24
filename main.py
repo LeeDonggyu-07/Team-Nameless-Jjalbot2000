8ac7ab3d29fb55caf5ffe71b16a27dc1d92db4c1
JcdeA <jcde@jcde.xyz>
2020-09-24 13:46:41 +0900

    add more and more features

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import discord
 import reddit #reddit.py
 from discord.ext import commands
 
-redditsc = reddit.redditsc
+#load posts from reddit
+redditsc = reddit.scrapper()
 
-game = discord.Game('!도움')
+subque = ['programmerhumor']
 
-bot = commands.Bot(command_prefix='!', status = discord.Status.online, activity = game, help_command = None)
+for i in subque:
+    redditsc.update(i)
+
+
+game = discord.Game('`짤')
+
+bot = commands.Bot(command_prefix='`', status = discord.Status.online, activity = game, help_command = None)
+
 
 @bot.event
 async def on_ready():
@@ -15,25 +23,40 @@ async def on_ready():
 
 @bot.command()
 async def 도움(ctx):
-    embed = discord.Embed(title= '짤봇2000를 사용해주셔서 감사합니다!', description=f'test123')
-    embed.add_field('짤봇 2000의 기능은 다음과 같습니다')
-    await ctx.send(embed)
-
-
-@bot.command()
-async def 짤(ctx):
-    sub, post = redditsc.random()
 
     embed = discord.Embed(
-        title = '레딧에서 온 짤:',
-        description ='r/'+ sub
+        title= '짤봇2000를 사용하주셔서 감사합니다.',
+        colour=discord.Colour(0xE5E242),
+
+        description = '테스트123',
     )
-    embed.set_image(url=post)
     
     await ctx.send(embed=embed)
 
 
+@bot.command(pass_context = True)
+async def 짤(ctx, arg = None):
+    if arg is None:
+        sub, url, details = redditsc.random()
 
+        print(arg)
+
+    else:
+        sub, url, details = redditsc.fromsub(arg)
+        print(url)
+        
+    embed = discord.Embed(
+            title = 'r/' +sub +' 에서 온 짤:',
+            description = details[0]
+    )
+    embed.set_image(url= url)
+    embed.url = 'https://reddit.com' + details[1]
+    embed.set_footer(
+        text  = ctx.message.author.name,
+        icon_url = ctx.message.author.avatar_url
+    )
+    
+    await ctx.send(embed=embed)
 
 
 bot.run(open('token').read())
--- reddit.py
+++ reddit.py
@@ -1,47 +1,79 @@
-import praw
-import random
-
+import praw, random, json
 
 reddit = praw.Reddit()
 
-
-class redditscrapper:
+class scrapper:
 
     def __init__(self):
-        self.storage = {}
-        self.used = []
-        self.posted = []
+        with open('db.json', 'r') as fp:
+            storage = json.load(fp)
+    
+        with open('postedurls.json', 'r') as fp:
+            posted = json.load(fp)
+
+    
+        self.storage = storage
+        self.posted = [posted]
 
 
 
-    def update(self,sub):
-        print('reddit: UPDATING POSTS..')
+    def update(self, sub , lmt = 100):
+        print('reddit.py: UPDATING POSTS..')
+
         subreddit = reddit.subreddit(sub)
         tmp = {}
-        for submission in subreddit.hot(limit=100):
-            tmp[submission.url] = [submission.title,submission.permalink]
+
+        for submission in subreddit.hot(limit= lmt):
+            tmp[submission.url] = [submission.title, submission.permalink ]
+        
         self.storage[sub] = tmp
-        print(self.storage)
-        print('\n\n')
+        
+        with open('db.json','w') as db:
+            json.dump(self.storage,db)
+
+        print('\n')
 
 
 
-    def random(self):
-        sub = random.choice(list(self.storage.keys()))
+    def random(self, subname = None):
+        ''' Returns and stores details of a random hot post from the sub requested.'''
+
+        if subname == None:
+            sub = random.choice(list(self.storage.keys()))
+
+        else: 
+            sub = subname
+
         posts = self.storage[sub]
-        post = random.choice(list(posts.keys()))
-        if post in self.used:
+        url = random.choice(list(posts.keys()))
+        details = posts[url]
+
+        if url in self.posted:
             self.random()
+
         else:
             print('sending post')
-            self.used.append(post)
-            return sub, post
+            self.posted.append(url)
+
+            with open('postedurls.json', 'w') as fp:
+                json.dump(self.posted, fp)
+
+            return sub, url, details
 
 
-redditsc = redditscrapper()
+    def stripsub(self,sub):
+        if sub.startswith('r/'):
+            return sub[2:]
+        
+        elif sub.startswith('/r/'):
+            return sub[3:]
 
-subque = ['programmerhumor','linuxmemes']
 
-for i in subque:
-    redditsc.update(i)
+    def fromsub(self, sub):
+        if self.stripsub(sub) in self.storage.keys():
+            return self.random(subname = sub)
+            
+        else:
+            self.update(sub, lmt=50)
+            return self.random(subname = sub)
 
